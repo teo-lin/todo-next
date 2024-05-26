5a19df3e85ad2243b5f5ab9f4b93a1f170ed832c
Teolin <teolin@gmail.com>
2024-05-26 20:28:01 +0300

    chore: sh

--- dir-structure.sh
+++ dir-structure.sh
@@ -1,10 +1,10 @@
-mkdir -p src/pages/api/users
-mkdir -p src/pages/api/tasks/[id]
-mkdir -p src/pages/api/lists
+mkdir -p src/pages/api/users/user
+mkdir -p src/pages/api/tasks/task/[id]
+mkdir -p src/pages/api/lists/list
 touch src/pages/api/users/register.ts
-touch src/pages/api/users/[id].ts
+touch src/pages/api/users/user/[id].ts
 touch src/pages/api/tasks/create.ts
-touch src/pages/api/tasks/[id].ts
-touch src/pages/api/tasks/[id]/complete.ts
+touch src/pages/api/tasks/task/[id].ts
+touch src/pages/api/tasks/task/[id]/complete.ts
 touch src/pages/api/lists/create.ts
-touch src/pages/api/lists/[id].ts
+touch src/pages/api/lists/list/[id].ts
